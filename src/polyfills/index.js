f790c363d225055c661b0c8f30254c6fdb68a210
seyilonge <seyilonge@gmail.com>
2019-12-06 15:35:47 -0500

    Polyfill updates, added IE11 for prod build

--- src/polyfills/index.js
+++ src/polyfills/index.js
@@ -1,6 +1,9 @@
 // External imports
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/symbol';
 import 'intersection-observer';
 
 // Local imports
